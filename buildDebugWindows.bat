560088e47208a1149d121f3fa261622079ddd1f6
VMan-2002 <vman.2002.au@gmail.com>
2022-05-13 16:03:47 +1000

    wooie

---
+++ buildDebugWindows.bat
@@ -0,0 +1,6 @@
+rem @echo off
+rem color 0a
+rem @echo on
+echo BUILDING GAME
+lime build windows -debug -D HXCPP_DEBUGGER
+pause
---
+++ buildReleaseWindows.bat
@@ -0,0 +1,6 @@
+@echo off
+color 0a
+@echo on
+echo BUILDING GAME
+lime build windows -release
+pause
