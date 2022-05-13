d11ed04e6d4066648bd24e244a520e0ed8eb50d9
VMan-2002 <vman.2002.au@gmail.com>
2022-05-13 18:20:40 +1000

    j

--- source/Paths.hx
+++ source/Paths.hx
@@ -142,4 +142,85 @@ class Paths
 	{
 		return FlxAtlasFrames.fromSpriteSheetPacker(image(key, library), file('images/$key.txt', library));
 	}
+	
+	/*mod shit*/
+	
+	#if MODS
+	public static var modsAvailable:Array<String> = [];
+	public static var modsActiveStor:Array<String> = [];
+	#else
+	public static var modsActiveStor:Array<String> = ["friday_night_funkin", "fnf_cornflower_week"];
+	#end
+	public static var modsActive:Array<String> = modsActiveStor;
+	
+	//awer[spc]789+
+	//idk lol
+	
+	public static function setPrimaryMod(name:String) {
+		if (modsActiveStor.indexOf(name) >= 0) {
+			modsActive = [name].concat(modsActiveStor.filter(function(a) {
+				return a != name;
+			}));
+		}
+		return modsActive[0];
+	}
+	
+	#if !MODS
+	inline
+	#end
+	public static function getModsList():Array<String> {
+		#if MODS
+		//check for txt exists
+		var txtThing:Array<String>;
+		var listChanged:Bool = false;
+		if (FileSystem.exists("modsActive.txt")) {
+			txtThing = File.getContent("modsActive.txt").split("\n");
+			listChanged = (txtThing.length == 1 && txtThing[0].length == 0);
+			if (listChanged) {
+				txtThing = [];
+			}
+		} else {
+			txtThing = new Array<String>();
+			listChanged = true;
+		}
+		var modsThing = new Map<String, Bool>();
+		modsActiveStor = [];
+		//understand it
+		for (i in txtThing) {
+			var a = i.split(":");
+			modsThing.set(a[1], a[0] == "1");
+			if (a[0] == "1") {
+				modsActiveStor.push(a[1]);
+			}
+		}
+		for (i in modsAvailable) {
+			if (!modsThing.exists(i)) {
+				modsThing.set(i, Options.newModsActive);
+				listChanged = true;
+			}
+		}
+		//be cool as fuck
+		if (listChanged) {
+			var newTxtThing = new Array<String>();
+			for (k in modsThing.keys()) {
+				newTxtThing.push('${modsThing.get(k) ? "1" : "0"}:${k}');
+			}
+			File.saveContent("modsActive.txt", newTxtThing.join("\n"));
+		}
+		#end
+		return modsActiveStor;
+	}
+	
+	#if MODS
+	public static function updateModsList() {
+		modsAvailable = FileSystem.readDirectory("mods").filter(function(a) {
+			return FileSystem.isDirectory('mods/$a');
+		});
+		return getModsList();
+	}
+	#else
+	public inline function updateModsList() {
+		return getModsList();
+	}
+	#end
 }
