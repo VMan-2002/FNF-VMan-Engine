ac2763e14e63ef81934d5ea5f336aeb2281b6c92
VMan-2002 <vman.2002.au@gmail.com>
2022-09-11 11:40:13 +1000

    Update Boyfriend.hx

--- source/Boyfriend.hx
+++ source/Boyfriend.hx
@@ -7,33 +7,23 @@ import flixel.util.FlxTimer;
 
 using StringTools;
 
-class Boyfriend extends Character
-{
-	public var stunned:Bool = false;
-
-	public function new(x:Float, y:Float, ?char:String = 'bf', ?modName:String)
-	{
+class Boyfriend extends Character {
+	public function new(x:Float, y:Float, ?char:String = 'bf', ?modName:String) {
 		super(x, y, char, true, modName);
 	}
 
-	override function update(elapsed:Float)
-	{
-		if (!debugMode)
-		{
-			if (animation.curAnim.name.startsWith('sing'))
-			{
+	override function update(elapsed:Float) {
+		if (!debugMode) {
+			if (animation.curAnim.name.startsWith('sing')) {
 				holdTimer += elapsed;
-			}
-			else
+			} else
 				holdTimer = 0;
 
-			if (animation.curAnim.name.endsWith('miss') && animation.curAnim.finished)
-			{
+			if (animation.curAnim.name.endsWith('miss') && animation.curAnim.finished) {
 				playAnim('idle', true, false, 10);
 			}
 
-			if (animation.curAnim.name == 'firstDeath' && animation.curAnim.finished)
-			{
+			if (animation.curAnim.name == 'firstDeath' && animation.curAnim.finished) {
 				playAnim('deathLoop');
 			}
 		}
