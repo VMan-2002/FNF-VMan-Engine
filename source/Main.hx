c8982e3e6cbf56e22d48b67c6324a80d67526d67
VMan-2002 <vman.2002.au@gmail.com>
2023-03-01 10:27:18 +1100

    game version

--- source/Main.hx
+++ source/Main.hx
@@ -18,6 +18,7 @@ import Sys;
 
 class Main extends Sprite
 {
+	public static final gameVersionInt = 2;
 	public static final gameVersionStr = "v1.0.1 Funny Day 0 Patch";
 
 	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
