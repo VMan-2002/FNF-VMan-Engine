3887493ed2501c4039df14b1a2b53a0c0be9a119
VMan-2002 <vman.2002.au@gmail.com>
2023-04-07 08:31:58 +1000

    cras

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -911,16 +911,17 @@ class PlayState extends MusicBeatState
 				["hits", "misses", "totalnotes", "health"]
 			];
 		} else {
-			/*hudThingLists = [
+			hudThingLists = [
 				["score", "misses", "fc", "accRating", "accSimple", "health"],
 				["song", "difficulty"],
 				["hits", "sicks", "goods", "bads", "shits", "misses", "totalnotes"]
-			];*/
-			hudThingLists = [];
+			];
+			//todo: fix crash
+			/*hudThingLists = [];
 			var hudThingAdds = Options.instance.hudThingInfo.split("\n");
 			while (hudThingAdds.length != 0) {
 				hudThingLists.push(hudThingAdds.shift().split(","));
-			}
+			}*/
 		}
 		hudThingLists[1].push("engine");
 		hudThings.add(new HudThing(healthBarBG.x, healthBarBG.y + 30, hudThingLists[0]));
